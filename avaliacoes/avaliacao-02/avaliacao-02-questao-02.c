aa98007ddf7db9e67ccbd61f4d4c829784d3756d
gabriellasilv4 <gabriella.silva.santos@aluno.ifce.edu.br>
2022-10-12 16:16:16 -0300

    avaliacao-03

--- avaliacoes/avaliacao-02/avaliacao-02-questao-02.c
+++ avaliacoes/avaliacao-02/avaliacao-02-questao-02.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+//Use um laço while para imprimir números pares múltiplos de 4. Use uma variável de controle onde seu
+//incremento é de uma unidade. Quando a variável for testada e não for múltiplo de 4 use o continue. Caso
+//contrário imprima o número.
+
 int main() {
   int i = 0;
   
